9a446c1b4f46326fc1ef3548dda333df8ecb230f
Leonid Shamis <lshamis@thirdwave.ai>
2019-08-08 17:42:35 -0700

    Wrap shmobj

---
+++ shmobj.go
@@ -0,0 +1,41 @@
+package alephzero
+
+// #cgo pkg-config: alephzero
+// #include <a0/shmobj.h>
+// #include <stdlib.h>
+import "C"
+
+import (
+	"unsafe"
+)
+
+type ShmObjOptions struct {
+	Size int
+}
+
+type ShmObj struct {
+	cShmObj C.a0_shmobj_t
+}
+
+func ShmOpen(path string, opts *ShmObjOptions) (so ShmObj, err error) {
+	pathCStr := C.CString(path)
+	defer C.free(unsafe.Pointer(pathCStr))
+
+	var cOpts C.a0_shmobj_options_t
+	if opts != nil {
+		cOpts.size = C.off_t(opts.Size)
+	}
+	err = errorFrom(C.a0_shmobj_open(pathCStr, &cOpts, &so.cShmObj))
+	return
+}
+
+func ShmUnlink(path string) error {
+	pathCStr := C.CString(path)
+	defer C.free(unsafe.Pointer(pathCStr))
+
+	return errorFrom(C.a0_shmobj_unlink(pathCStr))
+}
+
+func (so *ShmObj) Close() error {
+	return errorFrom(C.a0_shmobj_close(&so.cShmObj))
+}
